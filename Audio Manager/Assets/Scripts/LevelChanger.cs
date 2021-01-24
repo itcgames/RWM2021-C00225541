61cc33341fa506e65238feda4594f011a64ff8ef
Kevin Andersen <c00225541@itcarlow.ie>
2021-01-24 22:33:00 +0000

    [JIRA DE-15] Done

--- Audio Manager/Assets/Scripts/LevelChanger.cs
+++ Audio Manager/Assets/Scripts/LevelChanger.cs
@@ -6,12 +6,15 @@ public class LevelChanger : MonoBehaviour
 
     private string levelToLoad;
 
+    //activates the trigger to start fading
+    //sets the level string
     public void FadeToLevel (string level)
     {
         levelToLoad = level;
         animator.SetTrigger("FadeOut");
     }
 
+    //calls the scene manager to change screen
     public void OnFadeComplete()
     {
         FindObjectOfType<Manager>().ChangeScreen(levelToLoad);
